1e54964dc8c7c34a0f4ae568dcccdd0e833079a3
Aj-vrod <ammyvrodriguez@hotmail.com>
2024-01-30 21:17:39 +0100

    set up movies and movies/id handlers

--- main.go
+++ main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/GLML/pkg/server"
 )
-
-func startServer(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello, GLML\n")
-}
 
 func main() {
 	fmt.Println("Starting server...")
 	fmt.Println("Listening on port :8080")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", startServer).Methods("GET")
-
-	err := http.ListenAndServe(":8080", r)
-	if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
-	}
+	server.StartServer()
 }
