2bfa97da9114af9fd29ce920a9e5ca72325765a0
Aj-vrod <ammyvrodriguez@hotmail.com>
2024-02-05 21:10:13 +0100

    setup db connection

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/GLML/pkg/db"
 	"github.com/GLML/pkg/server"
 )
 
 func main() {
+	fmt.Println("Connecting to database...")
+	err := db.Connect()
+	if err != nil {
+		panic(err)
+	}
+	defer db.DB.Close()
+	fmt.Println("Successfully connected to database!")
+
 	fmt.Println("Starting server...")
 	fmt.Println("Listening on port :8080")
-
 	server.StartServer()
 }
